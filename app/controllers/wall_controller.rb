ad805b6ae805e9ece2c4d59d1d3e28b7b9145817
Codelion <Protorenaissance@gmail.com>
2015-03-04 11:49:57 +0000

    second second

--- app/controllers/wall_controller.rb
+++ app/controllers/wall_controller.rb
@@ -20,4 +20,15 @@ class WallController < ApplicationController
 	def edit
 		@post_edit=Post.find(params[:id])
 	end
+	def edit_complete
+		p=Post.find(params[:id])
+		p.name=params[:writer_edit]
+		p.content=params[:content_edit]
+		if p.save
+			redirect_to "/wall/posts"
+		else
+			flash[:alert]=p.errors[:content][0]
+			redirect_to :back
+		end
+	end
 end
