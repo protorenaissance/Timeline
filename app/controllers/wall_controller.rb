4d0f1643e311d212197e44e3981f64daef5b75c4
Codelion <Protorenaissance@gmail.com>
2015-03-05 07:30:52 +0000

    delete_complete page

--- app/controllers/wall_controller.rb
+++ app/controllers/wall_controller.rb
@@ -34,4 +34,9 @@ class WallController < ApplicationController
 	def delete
 		@post_delete=Post.find(params[:id])
 	end
+	def delete_complete
+		p=Post.find(params[:id])
+		p.destroy
+		redirect_to "/wall/posts"
+	end
 end
