3b9a4656a436d3e40b93973f0415894d8a6b56cf
Codelion <Protorenaissance@gmail.com>
2015-03-06 05:57:47 +0000

    Comment ended

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get 'wall/edit/:id'=>'wall#edit'
 	post 'wall/delete_complete'=>'wall#delete_complete'
 	get 'wall/write_comment/:id'=>'wall#write_comment'
+	get 'wall/write_comment_complete'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
